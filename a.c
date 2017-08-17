8d524b5d84cedad2242095158103c27d4978e494
www.yueqian.com.cn <gec@ubuntu.(none)>
2017-08-17 04:21:55 -0700

    vii脚本

---
+++ a.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <sys.socket.h>
+#include <stdbool.h>
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
---
+++ b.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <strings.h>
+#include <stdbool.h>
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
